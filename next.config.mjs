ed3cfe756751121d13d0d19edd4f4d8303b57344
Vineet Sharma <vineet9650022810@gmail.com>
2026-09-09 22:14:54 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  // Dangerously allow production builds to successfully complete even if
-  // your project has type errors.
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-
-  // Completely bypass ESLint checking during the production build step.
   eslint: {
     ignoreDuringBuilds: true,
   },
